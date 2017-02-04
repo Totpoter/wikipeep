e6652133a79ef5c5809df1c19003e1760597deb0
Lozius <nilsgarland@gmail.com>
2017-02-04 14:32:13 +0100

    Page routes set

--- index.php
+++ index.php
@@ -7,4 +7,21 @@ $app = new \Slim\Slim();
 $app->get('/hello/:name', function ($name) {
   echo "Hello, $name";
 });
+
+$app->get('/', function () {
+  echo "Start sidan";
+});
+
+$app->get('/bibliotek', function () {
+  echo "Biblioteket";
+});
+
+$app->get('/forum', function () {
+  echo "Forumet";
+});
+
+$app->get('/kurser', function () {
+  echo "Kurser";
+});
+
 $app->run();
