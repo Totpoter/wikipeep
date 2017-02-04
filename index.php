bd353789ce348495ea4a036158ffe22e728f728d
Lozius <nilsgarland@gmail.com>
2017-02-04 17:13:31 +0100

    Route navigation clarified

--- index.php
+++ index.php
@@ -6,18 +6,18 @@ $app = new \Slim\Slim();
 
 $app->get('/', function () use($app) {
   $app->render('index.php');
-});
+})->name('hem');
 
 $app->get('/bibliotek', function () use($app) {
   $app->render('library.php');
-});
+})->name('bibliotek');
 
 $app->get('/forum', function () use($app) {
   $app->render('forum.php');
-});
+})->name('forum');
 
 $app->get('/kurser', function () use($app) {
   $app->render('courses.php');
-});
+})->name('kurser');
 
 $app->run();
--- templates/index.php
+++ templates/index.php
@@ -1,3 +1,9 @@
 <?php include 'inc/header.inc.php'; ?>
 
+<br><br>
+
+<h1>Start sidan!</h1>
+<br>
+<a href="./forum?m=from_home">Forum</a>
+
 <?php include 'inc/footer.inc.php'; ?>
