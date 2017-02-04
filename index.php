9cf43aa14c6eafdf6b404af1fab2d56667059040
Lozius <nilsgarland@gmail.com>
2017-02-04 17:43:25 +0100

    indent

--- index.php
+++ index.php
@@ -4,20 +4,20 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
-$app->get('/', function () use($app) {
-  $app->render('index.php');
-})->name('hem');
+  $app->get('/', function () use($app) {
+    $app->render('index.php');
+  })->name('hem');
 
-$app->get('/bibliotek', function () use($app) {
-  $app->render('library.php');
-})->name('bibliotek');
+  $app->get('/bibliotek', function () use($app) {
+    $app->render('library.php');
+  })->name('bibliotek');
 
-$app->get('/forum', function () use($app) {
-  $app->render('forum.php');
-})->name('forum');
+  $app->get('/forum', function () use($app) {
+    $app->render('forum.php');
+  })->name('forum');
 
-$app->get('/kurser', function () use($app) {
-  $app->render('courses.php');
-})->name('kurser');
+  $app->get('/kurser', function () use($app) {
+    $app->render('courses.php');
+  })->name('kurser');
 
 $app->run();
